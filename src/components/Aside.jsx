1a01cdf7fc5b48c06cc52fffaa31d393c26af949
nantunggaputra <nantunggaputra@github.com>
2024-06-04 21:33:45 +0700

    Anime App

--- src/components/Aside.jsx
+++ src/components/Aside.jsx
@@ -1,7 +1,62 @@
 export default function Aside() {
+  const genres = [
+    "Action",
+    "Adventure",
+    "Comedy",
+    "Crime",
+    "Demons",
+    "Drama",
+    "Fantasy",
+    "Game",
+    "Historical",
+    "Horror",
+    "Isekai",
+    "Magic",
+    "Martial Arts",
+    "Mecha",
+    "Medical",
+    "Military",
+    "Music",
+    "Mystery",
+    "Mythology",
+    "Philosophical",
+    "Psychological",
+    "Parody",
+    "Police",
+    "Romance",
+    "Samurai",
+    "School",
+    "Sci-Fi",
+    "Seinen",
+    "Shoujo",
+    "Shounen",
+    "Slice of Life",
+    "Sports",
+    "Space",
+    "Superhero",
+    "Supernatural",
+    "Super Power",
+    "Thriller",
+    "Time Travel",
+    "Tragedy",
+    "Vampire",
+    "Wuxia",
+  ];
+
   return (
     <>
-      <div id="check" className="aside left"></div>
+      <div id="check" className="aside left">
+        <div className="check">
+          <h2>
+            <span style={{ color: "#0377b9" }}>Genre</span> and Anime
+          </h2>
+        </div>
+        <div className="genre-all">
+          {genres.map((genre) => (
+            <button key={genre}>{genre}</button>
+          ))}
+        </div>
+      </div>
       <div id="blog" className="aside right"></div>
     </>
   );
