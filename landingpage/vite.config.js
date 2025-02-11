dc225b0a96073a6fc3fd010d41a7957c31b298b2
nantunggaputra <nantunggaputra@github.com>
2025-02-12 00:36:07 +0700

    Anime App ft Landing Page - modified landing page url with subdomain mapping - maintenance version 0.0.0 beta

--- landingpage/vite.config.js
+++ landingpage/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -7,5 +8,13 @@ export default defineConfig({
   build: {
     outDir: '../weeboo/landingpage',
     emptyOutDir: true,
-  }
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+      },
+    },
+  },
+  esbuild: {
+    minify: true,
+  },
 })
