2f70f98605915057d330e83c51e6c44269e13f00
nantunggaputra <nantunggaputra@github.com>
2025-02-22 23:27:24 +0700

    Anime App ft Landing Page - update vite.config.js (add obfuscation with terser) - maintenance version 0.1.0 beta

--- landingpage/vite.config.js
+++ landingpage/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import obfuscator from 'rollup-plugin-obfuscator'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -17,4 +18,18 @@ export default defineConfig({
   esbuild: {
     minify: true,
   },
+  rollupOptions: {
+    plugins: [
+      obfuscator({
+        rotateStringArray: true,
+        stringArray: true,
+        stringArrayThreshold: 0.75,
+        compact: true,
+        controlFlowFlattening: true,
+        debugProtection: true,
+      }),
+    ],
+  },
+  sourcemap: false,
+  minify: 'terser',
 })
